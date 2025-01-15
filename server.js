cdcd1eda11fbabcce8a587fb6385df98baec3eb3
Farah Stumpf <farahpst@byui.edu>
2025-01-15 16:51:57 -0700

    Changes to server, layout and footer

--- server.js
+++ server.js
@@ -2,40 +2,44 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+
+/* ***********************
+ * Middleware
+ *************************/
+app.use(express.static("public")); 
+app.use(expressLayouts);
 
 /* ***********************
  * Routes
  *************************/
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("index", { title: "Home" });
 });
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout.ejs") // not at views root
+app.set("view engine", "ejs");
+app.set("layout", "./layouts/layout.ejs"); // Path to layout
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT;
-const host = process.env.HOST;
+const port = process.env.PORT || 5500;
+const host = process.env.HOST || "localhost";
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`App listening on ${host}:${port}`);
+  console.log(`App listening on http://${host}:${port}`);
 });
-
-
